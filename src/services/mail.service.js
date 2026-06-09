def930eeeabc33c5ecc65b561b6ab66e09592cbb
Sourabh Yadav <sourabhyadav09902@gmail.com>
2026-06-09 21:29:42 +0530

    fix oauth cookie

--- src/services/mail.service.js
+++ src/services/mail.service.js
@@ -1,3 +1,26 @@
+import nodemailer from "nodemailer";
+
+const transporter =
+  nodemailer.createTransport({
+
+    host: "smtp.gmail.com",
+
+    port: 587,
+
+    secure: false,
+
+    auth: {
+
+      user:
+        process.env.EMAIL_USER,
+
+      pass:
+        process.env.EMAIL_PASS
+
+    }
+
+  });
+
 export const sendOtpEmail =
   async (email, otp) => {
 
@@ -36,7 +59,7 @@ export const sendOtpEmail =
 
       console.log(
         "Email Error:",
-        error.message
+        error
       );
 
       throw error;
