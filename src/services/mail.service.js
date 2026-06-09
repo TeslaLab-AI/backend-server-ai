2763ebdeff412a59d73746bddacbf0f8b4ab55a0
Sourabh Yadav <sourabhyadav09902@gmail.com>
2026-06-10 02:00:59 +0530

    increase smtp timeout

--- src/services/mail.service.js
+++ src/services/mail.service.js
@@ -67,3 +67,11 @@ export const sendOtpEmail = async (email, otp) => {
   }
 
 };
+
+console.log("EMAIL_HOST:", process.env.EMAIL_HOST);
+console.log("EMAIL_PORT:", process.env.EMAIL_PORT);
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log(
+  "EMAIL_PASS exists:",
+  !!process.env.EMAIL_PASS
+);
